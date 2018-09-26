c52022ca10c37b323de00907935e558afbcac312
Vinicius Campos <viniciuscampos120@gmail.com>
2018-09-26 10:08:44 -0300

    Update parser

--- compiler/syntactic-analyzer/bottom-up/run.sh
+++ compiler/syntactic-analyzer/bottom-up/run.sh
@@ -1,3 +1,7 @@
 yacc -d src/parser.y
 mv y.tab.c src/
 mv y.tab.h include/
+lex -o src/lex.yy.c src/lexical.l
+cc -o bin/parser src/y.tab.c src/lex.yy.c -I include/ -ly -ll
+rm src/lex.yy.c src/y.tab.c include/y.tab.h
+
