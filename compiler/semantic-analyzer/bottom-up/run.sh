e51dbae9694051acd3e2bc85c36280dadbc364a3
Vinicius Campos <viniciuscampos120@gmail.com>
2018-11-14 06:45:10 -0300

    Revert "Start to implement final work"
    This reverts commit 299d861b3469c789f993f369465c590390510454.

--- compiler/semantic-analyzer/bottom-up/run.sh
+++
@@ -1,9 +0,0 @@
-yacc -d src/parser.y
-mv y.tab.c src/
-mkdir -p include
-mv y.tab.h include/
-flex -o src/lex.yy.c src/lexical.l
-mkdir -p bin
-g++ -std=c++11 -o bin/parser src/y.tab.c src/lex.yy.c -I include/ -ly
-rm src/lex.yy.c src/y.tab.c include/y.tab.h
-
